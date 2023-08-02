b82ffb98985500f8d441080066a7fa1b02af3931
alexomur <alexomur111@gmail.com>
2023-08-02 15:03:23 +0300

    Finished `.items` command...

--- SCP049Items/Commands/Items.cs
+++ SCP049Items/Commands/Items.cs
@@ -1,5 +1,15 @@
 using System;
 using CommandSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandSystem;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Pickups;
+using InventorySystem.Items.Pickups;
+using PlayerRoles;
+using UnityEngine;
 
 namespace SCP049Items.Commands
 {
@@ -8,7 +18,23 @@ namespace SCP049Items.Commands
     {
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            throw new NotImplementedException();
+            Player player = Player.Get(sender);
+            if (player.Role != RoleTypeId.Scp049)
+            {
+                response = "You need to be SCP-049 first";
+                return false;
+            }
+
+            List<Item> items = player.Items.ToList();
+
+            response = "";
+            for (int i = 1; i <= items.Count; i++)
+            {
+                response += "\n[" + i + "] " + items[i-1].Type;
+            }
+
+            return true;
+
         }
 
         public string Command { get; } = "Items";
